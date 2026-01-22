39158d6ea47e40eafa7778f94f40e611153af27b
MuratYucedag <yucedagmurat23@gmail.com>
2026-01-22 15:50:33 +0300

    ....

---
+++ ResumeProjectDemo/Controllers/PortfolioController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResumeProjectDemo.Context;
+using ResumeProjectDemo.Entities;
+
+namespace ResumeProjectDemo.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        private readonly ResumeContext _context;
+        public PortfolioController(ResumeContext context)
+        {
+            _context = context;
+        }
+        public IActionResult PortfolioList()
+        {
+            var values = _context.Portfolios.ToList();
+            return View(values);
+        }
+
+        public IActionResult CreatePortfolio()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatePortfolio(Portfolio portfolio)
+        {
+            portfolio.ImageUrl = "test";
+            _context.Portfolios.Add(portfolio);
+            _context.SaveChanges();
+            return RedirectToAction("PortfolioList");
+        }
+    }
+}
